ff49241f5b93033881bec8a0b0a6c6e52bb749d6
Yarek Tyshchenko <yarek.tyshchenko@affiliatewindow.com>
2012-03-19 10:09:34 +0000

    - Fix a typo in joiner and some formatting in reloader

--- joiner.py
+++ joiner.py
@@ -1,10 +1,11 @@
 def message(pryme, message, source, target):
     if pryme.nick in message.pop(0):
         action = message.pop(0)
-        channel = message.pop(0)
         if "join" in action:
+            channel = message.pop(0)
             pryme.debug("Joining channel: " + channel)
             pryme.server.join(channel)
-        if "part" in message:
-            pryme.debug("Leaving channel: " + channel)
-            pryme.server.part(channel)
+        
+        if "part" in action:
+            pryme.debug("Leaving channel: " + target)
+            pryme.server.part(target)
--- reloader.py
+++ reloader.py
@@ -1,7 +1,7 @@
 def message(pryme, message, source, target):
-    if source not in pryme.adminNick:
-        return
-	
+    #if source not in pryme.adminNick:
+    #    return
+    
     if pryme.nick in message.pop(0):
         if "reload" in message.pop(0):
             for module in pryme.modules:
