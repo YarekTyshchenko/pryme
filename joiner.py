e08b5863f42576ab28e9815b0c4efce9919f3192
Yarek Tyshchenko <yarek.tyshchenko@affiliatewindow.com>
2012-03-22 15:46:44 +0000

    - Only allow adminNick to reload modules and add new modules

--- joiner.py
+++ joiner.py
@@ -1,11 +1,12 @@
 def message(pryme, message, source, target):
-    if pryme.nick in message.pop(0):
-        action = message.pop(0)
-        if "join" in action:
-            channel = message.pop(0)
-            pryme.debug("Joining channel: " + channel)
-            pryme.server.join(channel)
-        
-        if "part" in action:
-            pryme.debug("Leaving channel: " + target)
-            pryme.server.part(target)
+    if source in pryme.adminNick:
+        if pryme.nick in message.pop(0):
+            action = message.pop(0)
+            if "join" in action:
+                channel = message.pop(0)
+                pryme.debug("Joining channel: " + channel)
+                pryme.server.join(channel)
+            
+            if "part" in action:
+                pryme.debug("Leaving channel: " + target)
+                pryme.server.part(target)
--- loader.py
+++ loader.py
@@ -1,9 +1,8 @@
 def message(pryme, message, source, target):
-    if source not in pryme.adminNick:
-        return
-    if pryme.nick in message.pop(0):
-        if "addmodule" in message.pop(0):
-            moduleName = message.pop(0)
-            print 'Importing ' + moduleName
-            module = __import__(moduleName)
-            pryme.modules.append(module)
+    if source in pryme.adminNick:
+        if pryme.nick in message.pop(0):
+            if "addmodule" in message.pop(0):
+                moduleName = message.pop(0)
+                print 'Importing ' + moduleName
+                module = __import__(moduleName)
+                pryme.modules.append(module)
